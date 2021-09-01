a9069a63d0770045e837b289fff1ccdc8d9a82d1
natecheney-dev <natecheney@gmail.com>
2021-09-01 17:36:57 -0500

    finished mvp projects

--- components/Menu.js
+++ components/Menu.js
@@ -8,20 +8,61 @@ let menuItems = [
   'Music',
   'Log Out'
 ];
+  // Step 1: Write a component called 'menuMaker' to create a menu like the markup below:
+
+
+  // <div class="menu">
+  //   <ul>
+  //     {each menu item as an <li>}
+  //   </ul>
+  // </div>
+
+  // The 'menuMaker' takes an array of menu items as its only argument.
+
+const header = document.querySelector('.header');
+
+function menuMaker(data) {
+    const menu = document.createElement('div');
+    const listBullet = document.createElement('ul');
+    menu.className = 'menu';
+  // Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
+  // Add those items to the <ul>
+  menuItems.forEach(item => {
+    const links = document.createElement('li');
+    listBullet.appendChild(links);
+    links.textContent = item;
+  })
+
+  menu.appendChild(listBullet);
+    
+  const menuButton = document.querySelector('.menu-button');
+
+  menuButton.addEventListener('click', () => {
+    menu.classList.toggle('menu--open');
+  })
+
+  return menu;
+
+}
+
+menuMaker(menuItems);
+
+const menuStuff = menuItems.map(itemElem => {
+  return menuMaker(itemElem);
+})
+
+menuStuff.forEach(itemElems => {
+  header.appendChild(itemElems);
+})
+
+
+
+
+
+
 
 /* 
-  Step 1: Write a component called 'menuMaker' to create a menu like the markup below:
-
-  <div class="menu">
-    <ul>
-      {each menu item as an <li>}
-    </ul>
-  </div>
-
-  The 'menuMaker' takes an array of menu items as its only argument.
-
-  Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
-  Add those items to the <ul>
+  
 
   Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
 
